91d5f2208395db7b856317e518fce7c98b210bb6
Kyumin Kim <math4tots@users.noreply.github.com>
2020-06-05 17:20:10 -0700

    Class().get_method()

--- src/base/globals/nclss/cls.rs
+++ src/base/globals/nclss/cls.rs
@@ -1,16 +1,40 @@
 use crate::Class;
 use crate::ClassKind;
 use crate::SymbolRegistryHandle;
-
+use crate::NativeFunction;
+use crate::Value;
+use crate::Eval;
 use std::collections::HashMap;
 use std::rc::Rc;
 
-pub(super) fn mkcls(_symbol_registry: &SymbolRegistryHandle, base: Rc<Class>) -> Rc<Class> {
+pub(super) fn mkcls(sr: &SymbolRegistryHandle, base: Rc<Class>) -> Rc<Class> {
+    let methods = vec![
+        NativeFunction::snew(sr, "get_method", (&["self", "name"], &[("default", Value::Uninitialized)], None, None), |globals, args, _kwargs| {
+            let cls = Eval::expect_class(globals, &args[0])?;
+            let method_name = Eval::expect_symbollike(globals, &args[1])?;
+            match cls.get_from_instance_map(&method_name) {
+                Some(method) => Ok(method.clone()),
+                None => if let Value::Uninitialized = &args[2] {
+                    globals.set_exc_str(&format!(
+                        "Method {} not found for class {}",
+                        method_name,
+                        cls.full_name(),
+                    ))
+                } else {
+                    Ok(args[2].clone())
+                }
+            }
+        }),
+    ]
+    .into_iter()
+    .map(|f| (sr.intern_rcstr(f.name()), Value::from(f)))
+    .collect();
+
     Class::new0(
         ClassKind::NativeClass,
         "Class".into(),
         vec![base],
-        HashMap::new(),
+        methods,
         HashMap::new(),
     )
     .into()
