39ea0d80501f54145627c5f73a4825cb516ec0c2
Kyumin Kim <math4tots@users.noreply.github.com>
2020-06-12 11:29:08 -0700

    move embedded scripts out into *.u files with include_str

--- src/base/emb/mod.rs
+++ src/base/emb/mod.rs
@@ -2,13 +2,11 @@
 //! These are scripts that could live separately, but seem important
 //! enough that they should be embedded directly into the source
 
-mod os;
-mod prel;
 use crate::Globals;
 
 pub(super) fn install_embedded_sources(globals: &mut Globals) {
-    add(globals, "__prelude", prel::SOURCE);
-    add(globals, "os", os::SOURCE);
+    add(globals, "__prelude", include_str!("prel.u"));
+    add(globals, "os", include_str!("os.u"));
 }
 
 fn add(globals: &mut Globals, name: &'static str, data: &'static str) {
