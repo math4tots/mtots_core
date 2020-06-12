99ea147be8c713752ee03a07f833daa2062130b5
Kyumin Kim <math4tots@users.noreply.github.com>
2020-06-12 11:58:26 -0700

    qualify all standard library modules with 'a.'

--- src/base/emb/mod.rs
+++ src/base/emb/mod.rs
@@ -6,7 +6,7 @@ use crate::Globals;
 
 pub(super) fn install_embedded_sources(globals: &mut Globals) {
     add(globals, "__prelude", include_str!("prel.u"));
-    add(globals, "os", include_str!("os.u"));
+    add(globals, "a.os", include_str!("os.u"));
 }
 
 fn add(globals: &mut Globals, name: &'static str, data: &'static str) {
