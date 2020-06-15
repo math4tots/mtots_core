d42f4c7a4018c8758ce176b53802e0314ce6f827
Kyumin Kim <math4tots@users.noreply.github.com>
2020-06-14 23:25:40 -0700

    Bytes().len()

--- src/base/globals/nclss/bytes.rs
+++ src/base/globals/nclss/bytes.rs
@@ -8,23 +8,35 @@ use crate::Value;
 use std::rc::Rc;
 
 pub(super) fn mkcls(sr: &SymbolRegistryHandle, base: Rc<Class>) -> Rc<Class> {
-    let methods = vec![NativeFunction::simple0(
-        sr,
-        "decode",
-        &["self", "encoding"],
-        |globals, args, _kwargs| {
-            let bytes = Eval::expect_bytes(globals, &args[0])?;
-            let encoding = Eval::expect_symbol(globals, &args[1])?;
-            if encoding == Symbol::UTF8 {
-                match std::str::from_utf8(bytes) {
-                    Ok(s) => Ok(s.into()),
-                    Err(error) => globals.set_utf8_error(error),
+    let methods = vec![
+        NativeFunction::simple0(
+            sr,
+            "decode",
+            &["self", "encoding"],
+            |globals, args, _kwargs| {
+                let bytes = Eval::expect_bytes(globals, &args[0])?;
+                let encoding = Eval::expect_symbol(globals, &args[1])?;
+                if encoding == Symbol::UTF8 {
+                    match std::str::from_utf8(bytes) {
+                        Ok(s) => Ok(s.into()),
+                        Err(error) => globals.set_utf8_error(error),
+                    }
+                } else {
+                    globals.set_exc_str(&format!("Unrecognized encoding {:?}", encoding,))
                 }
-            } else {
-                globals.set_exc_str(&format!("Unrecognized encoding {:?}", encoding,))
-            }
-        },
-    )]
+            },
+        ),
+        NativeFunction::sdnew(
+            sr,
+            "len",
+            (&["self"], &[], None, None),
+            None,
+            |globals, args, _kwargs| {
+                let bytes = Eval::expect_bytes(globals, &args[0])?;
+                Ok((bytes.len() as i64).into())
+            },
+        ),
+    ]
     .into_iter()
     .map(|f| (sr.intern_rcstr(f.name()), Value::from(f)))
     .collect();
