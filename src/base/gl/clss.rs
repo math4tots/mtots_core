9ac9fd91e6d430cc75356ca7078c305832e0bcfa
Kyumin Kim <math4tots@users.noreply.github.com>
2020-08-07 13:45:04 -0700

    comparison binops

--- src/base/gl/clss.rs
+++ src/base/gl/clss.rs
@@ -20,7 +20,72 @@ pub struct ClassManager {
 impl ClassManager {
     #[allow(non_snake_case)]
     pub(super) fn new() -> Self {
-        let Nil = Class::new("Nil".into(), HashMap::new(), HashMap::new());
+        let Nil = Class::new("Nil".into(), Class::map_from_funcs(vec![
+            NativeFunction::new(
+                "map",
+                ["self", "x", "f"],
+                "",
+                |globals, args, _| {
+                    let mut args = args.into_iter();
+                    args.next().unwrap();  // nil
+                    let x = args.next().unwrap();
+                    let f = args.next().unwrap();
+                    if x.is_nil() {
+                        Ok(Value::Nil)
+                    } else {
+                        f.apply(globals, vec![x], None)
+                    }
+                }
+            ),
+            NativeFunction::new(
+                "vmap",
+                ["self", "x", "new"],
+                "",
+                |_globals, args, _| {
+                    let mut args = args.into_iter();
+                    args.next().unwrap();  // nil
+                    let x = args.next().unwrap();
+                    let new = args.next().unwrap();
+                    if x.is_nil() {
+                        Ok(Value::Nil)
+                    } else {
+                        Ok(new)
+                    }
+                }
+            ),
+            NativeFunction::new(
+                "get",
+                ["self", "x", "default"],
+                "",
+                |_globals, args, _| {
+                    let mut args = args.into_iter();
+                    args.next().unwrap();  // nil
+                    let x = args.next().unwrap();
+                    let default = args.next().unwrap();
+                    if x.is_nil() {
+                        Ok(default)
+                    } else {
+                        Ok(x)
+                    }
+                }
+            ),
+            NativeFunction::new(
+                "fget",
+                ["self", "x", "f"],
+                "",
+                |globals, args, _| {
+                    let mut args = args.into_iter();
+                    args.next().unwrap();  // nil
+                    let x = args.next().unwrap();
+                    let f = args.next().unwrap();
+                    if x.is_nil() {
+                        f.apply(globals, vec![], None)
+                    } else {
+                        Ok(x)
+                    }
+                }
+            ),
+        ]), HashMap::new());
         let Bool = Class::new("Bool".into(), HashMap::new(), HashMap::new());
         let Number = Class::new("Number".into(), HashMap::new(), HashMap::new());
         let String = Class::new(
