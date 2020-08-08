b859a29a0eb28cf2cac26accfda7db420c95b1d8
Kyumin Kim <math4tots@users.noreply.github.com>
2020-08-07 21:12:08 -0700

    Iterator.enumerate

--- src/base/gl/clss/iter.rs
+++ src/base/gl/clss/iter.rs
@@ -26,6 +26,32 @@ pub(super) fn new(iterable: &Rc<Class>) -> Rc<Class> {
                         Ok(Value::from(owner.unpack(globals)?))
                     },
                 ),
+                NativeFunction::new(
+                    "enumerate",
+                    ArgSpec::builder().req("self").def("start", 0),
+                    "Converts this iterator into a list",
+                    |_globals, args, _| {
+                        let mut args = args.into_iter();
+                        let owner = args.next().unwrap();
+                        let mut i = args.next().unwrap().number()?;
+                        Ok(NativeGenerator::new(
+                            "Iterator.enumerate",
+                            move |globals, arg| {
+                                match owner.resume(globals, arg) {
+                                    ResumeResult::Yield(value) => {
+                                        let n = i;
+                                        i += 1.0;
+                                        ResumeResult::Yield(vec![
+                                            Value::from(n),
+                                            value,
+                                        ].into())
+                                    }
+                                    r => r,
+                                }
+                            },
+                        ).into())
+                    },
+                ),
             ]),
             vec![iterable],
         ),
